984c64dc584c5ba692fb1fec8dd6fe4c853c4e16
hanzhen <han123zhen123@163.com>
2026-04-06 20:05:51 +0800

    x

--- pages/football/index.vue
+++ pages/football/index.vue
@@ -246,7 +246,7 @@ selectedMatchCount() {
     this.calcHeaderHeight();
     this.calcPopupMaxHeight();
   },
-  onShow() {
+  onLoad() {
       // 强制显示分享菜单，立刻解除置灰
   wx.showShareMenu({
     menus: ['shareAppMessage', 'shareTimeline']
