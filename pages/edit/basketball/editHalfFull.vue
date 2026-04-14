848d58952bfa0dfc61987ee365a18abb2abbfdb7
hanzhen <han123zhen123@163.com>
2026-04-14 14:08:55 +0800

    x

--- pages/edit/basketball/editHalfFull.vue
+++ pages/edit/basketball/editHalfFull.vue
@@ -80,9 +80,9 @@
       </view>
       <view class="bet-bar-bottom">
         <view class="bottom-middle">
-         <text class="select-tip">共{{ betNotes }}注 {{ betCount }}倍 {{ totalBetAmount }}￥</text>
+         <text class="select-tip">共{{ betNotes }}注 {{ betCount }}倍 {{ totalBetAmount }}</text>
           <text class="bonus-tip">
-            {{ calculateBonusText() }}￥
+            {{ calculateBonusText() }}
             </text>
         </view>
       </view>
