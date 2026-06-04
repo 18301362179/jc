7e250d9e6395b258c0b4afb4e41f7b2e6aca0b5b
hanzhen <han123zhen123@163.com>
2026-06-05 00:44:52 +0800

    x

--- pages/test/basketballPredict.vue
+++ pages/test/basketballPredict.vue
@@ -358,7 +358,7 @@
         </view>
 
         <!-- 客队数据 away -->
-        <view class="team-status">
+        <view class="team-status" style="margin-bottom: 40rpx;">
           <view class="record-summary">
             <text class="team-tag-sm">{{ (recentData && recentData.away && recentData.away.statistics && recentData.away.statistics.teamShortName) || "0" }}</text>
             近{{ (recentData && recentData.away && recentData.away.statistics && recentData.away.statistics.totalLegCnt) || "0" }}场
@@ -879,7 +879,7 @@ export default {
 }
 
 .scroll-content {
-  height: calc(100vh - 380rpx);
+  height: calc(100vh - 400rpx);
   box-sizing: border-box;
 }
 
--- pages/test/footballPredict.vue
+++ pages/test/footballPredict.vue
@@ -655,7 +655,7 @@
             <view class="no-data-card" v-if="((injuryData && injuryData.home && injuryData.home.injuriesAndSuspensionsList) || []).length === 0">暂无伤停</view>
           </scroll-view>
         </view>
-        <view class="player-group">
+        <view class="player-group" style="margin-bottom: 40rpx;">
           <view class="team-tag-sm">{{ (injuryData && injuryData.away && injuryData.away.teamShortName) || "0" }}</view>
           <scroll-view class="player-scroll" scroll-x show-scrollbar="false">
             <view class="injury-card" v-for="(item, idx) in (injuryData && injuryData.away && injuryData.away.injuriesAndSuspensionsList) || []" :key="idx">
@@ -1220,7 +1220,7 @@ export default {
 }
 
 .scroll-content {
-  height: calc(100vh - 380rpx);
+  height: calc(100vh - 400rpx);
   box-sizing: border-box;
 }
 
