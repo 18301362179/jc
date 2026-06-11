81a8e3e55ed4f52ca6441c329d3f146de504fca3
hanzhen <han123zhen123@163.com>
2026-06-11 09:10:15 +0800

    x

---
+++ pages/test/myTest.vue
@@ -0,0 +1,36 @@
+<template>
+  <view class="page-container">
+    <!-- 固定头部 -->
+    <CustomHeader :title="'预测数据'" :showBack="true" :isIndex="false" :showIcon="false" :isSelected="false" :selectedPlay="''" @funnel-click="handleFunnel" @back-click="onBackClick" />
+    <view class="test">
+      <view class="c1"></view>
+      <view class="c2"></view>
+    </view>
+  </view>
+</template>
+
+<script>
+import CustomHeader from "@/components/CustomHeader.vue";
+export default {
+  components: { CustomHeader },
+  data() {
+    return {
+    };
+  },
+
+};
+</script>
+
+<style lang="scss" scoped>
+.test {
+  width: 100%;
+  height: 200rpx;
+  background:gray;
+  .c1{
+    float:left;
+    width: 50%;
+    height: 50rpx;
+    background:green;
+  }
+}
+</style>
